9f0b096656fd0d51e747a9677fecc77ee314c72d
RNAV2022 <ryannav2019@gmail.com>
2023-06-08 12:22:32 +0100

    changed to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-import deno from "@astrojs/deno";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server",
-  adapter: deno()
+  adapter: vercel()
 });
